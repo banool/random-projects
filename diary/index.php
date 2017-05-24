a407e29e7c7173b88a9814a90009b7b45d94cf34
Daniel Porteous <danielporteous1@gmail.com>
2017-05-25 03:43:43 +1000

    Adding a count of the number of entries

--- diary/index.php
+++ diary/index.php
@@ -1,24 +1,26 @@
 <?php
 echo '<meta name="robots" content="noindex,nofollow">';
-echo "<b>Entries</b></br></br>";
 $path = "./entries";
 $dh = opendir($path);
-$files = array();
+$entriess = array();
 $others = array();
 while(false != ($file = readdir($dh))) {
     if($file != "." && $file != ".." && $file != "index.php" && $file != "github-markdown.css" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
         if(ereg("^[0-9]{4}-[0-9]{2}-[0-9]{2}.html$", $file)){
-            $files[] = $file;
+            $entries[] = $file;
         } else {
             $others[] = $file;
         }
     }
 }
 
-natsort($files);
+$numentries = count($entries);
+echo "<b>Entries ($numentries)</b></br></br>";
+
+natsort($entries);
 natsort($others);
 
-foreach($files as $file) {
+foreach($entries as $file) {
     $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
     echo "<a href='$path/$file'>$withoutExt</a><br /><br />";
 }
